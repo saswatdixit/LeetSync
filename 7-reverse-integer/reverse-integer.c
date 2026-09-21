0ce6ddd94341166ea86094c8121393833f14c4f8
Saswat Dixit <85784995+saswatdixit@users.noreply.github.com>
2026-09-21 19:34:08 +0530

    Time: 3 ms (55.33%) | Memory: 9.4 MB (8.67%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,20 @@
+int reverse(int x) {
+    int result = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        x = x / 10;
+
+        if (result > 214748364 ||
+            (result == 214748364 && digit > 7))
+            return 0;
+
+        if (result < -214748364 ||
+            (result == -214748364 && digit < -8))
+            return 0;
+
+        result = result * 10 + digit;
+    }
+
+    return result;
+}
